e43e94f895c3b29ef900fcd5410d396008c7a5b6
Orest Ivasiv <orest.ivasiv@zalando.de>
2022-12-05 18:01:57 +0100

    Added simple handler

--- wiki.go
+++ wiki.go
@@ -1,8 +1,12 @@
+// go:build ignore
+
 package main
 
 import (
-	"os"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 type Page struct {
@@ -12,25 +16,28 @@ type Page struct {
 
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
-
-	// The octal integer literal 0600, passed as the third parameter to WriteFile,
-	// indicates that the file should be created with read-write permissions for the current user only.
-	// (See the Unix man page open(2) for details.)
 	return os.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
-    body, err := os.ReadFile(filename)
-    if err != nil {
-        return nil, err
-    }
-    return &Page{Title: title, Body: body}, nil
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 func main() {
-    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-    p1.save()
-    p2, _ := loadPage("TestPage")
-    fmt.Println(string(p2.Body))
+	p1 := &Page{Title: "test", Body: []byte("This is a sample Page.")}
+	p1.save()
+
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
